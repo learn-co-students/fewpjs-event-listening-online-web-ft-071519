9d97a87216fc625dffb9b25d47ea774c9efcaf03
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-31 19:44:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+
+const input = document.querySelector('input')
+
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    alert("I was clicked!")
+  })
 }
+
+
+const main = document.querySelector('main')
+
+main.addEventListener('click', function(event){
+  alert("http://youtube.com")
+})
